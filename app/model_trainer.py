77d9406aa3c3fef1bf740c9b22970d4421552a6a
phperesdasilva <phperesdasilva@gmail.com>
2026-05-12 10:09:10 -0300

    model trainer sendo loggado

--- app/model_trainer.py
+++ app/model_trainer.py
@@ -9,7 +9,8 @@ from dataset import DatasetManager
 from lstm import StockLSTM
 from torch.utils.data import TensorDataset,DataLoader
 
-mlflow.pytorch.autolog()
+MLFLOW_TRACKING_URI = 'http://localhost:3050'
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
 ticker = 'PETR4.SA'
 years = 5
@@ -19,6 +20,8 @@ device =  torch.device("cuda" if torch.cuda.is_available() else "cpu")
 hidden_size = 32
 num_layers = 2
 dropout = 0.2
+
+learning_rate = 0.001
 
 prediction_period = 15
 lookback_period = 60
@@ -30,7 +33,7 @@ dataset = DatasetManager(ticker=ticker, years=years)
 
 df = dataset.download_data()
 
-features_to_keep = ['Date', 'Close']
+features_to_keep = ['Close']
 df = dataset.filter_features(df, features_to_keep)
 
 features = dataset.get_features(df)
@@ -56,48 +59,64 @@ model = StockLSTM(
 ).to(device)
 
 criterion = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 train_dataset = TensorDataset(x_train_tensor, y_train_tensor)
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-for epoch in range(200):
-    epoch_loss = 0.0
-    num_batches = 0
-    for X_batch, y_batch in train_loader:
-        X_batch = X_batch.to(device)
-        y_batch = y_batch.to(device)
-        optimizer.zero_grad()
-        y_pred = model(X_batch)
-        loss = criterion(y_pred, y_batch)
-        loss.backward()
-        optimizer.step()
-        epoch_loss +=loss.item()
-        num_batches +=1
-    epoch_loss_avg = epoch_loss / num_batches
-    if (epoch + 1) % 10 == 0:
+mlflow.enable_system_metrics_logging()
+
+with mlflow.start_run(run_name='Stock Prediction'):
+
+    mlflow.log_params({"Learning Rate": learning_rate, 
+                       "Batch Size": batch_size, 
+                       "Epochs": epochs,
+                       "Hidden Size": hidden_size,
+                       "Number of Layers": num_layers,
+                       "Dropout": dropout})
+
+    for epoch in range(epochs):
+        epoch_loss = 0.0
+        num_batches = 0
+        for x_batch, y_batch in train_loader:
+            x_batch = x_batch.to(device)
+            y_batch = y_batch.to(device)
+            optimizer.zero_grad()
+            y_pred = model(x_batch)
+            loss = criterion(y_pred, y_batch)
+            loss.backward()
+            optimizer.step()
+            epoch_loss +=loss.item()
+            num_batches +=1
+
+        epoch_loss_avg = epoch_loss / num_batches
         print(f'Epoch {epoch + 1}, Loss: {epoch_loss_avg:.4f}')
 
-model.eval()
+        mlflow.log_metric("loss", loss.item(), step=epoch)
 
-with torch.no_grad():
-    y_pred_test = model(x_test_tensor.to(device))
+    mlflow.pytorch.log_model(model, name="Stock Prediction LSTM")
 
-y_pred_test = y_pred_test.cpu().numpy()
+    model.eval()
 
-y_train_real = dataset.inverse_transform(y_train)
-y_pred_real = dataset.inverse_transform(y_pred_test)
-y_test_real = dataset.inverse_transform(y_test)
+    with torch.no_grad():
+        y_pred_test = model(x_test_tensor.to(device))
 
-mape = mean_absolute_percentage_error(y_test_real, y_pred_real)
-print(f'MAPE: {mape:.4f}')
+    y_pred_test = y_pred_test.cpu().numpy()
 
-mae = mean_absolute_error(y_test_real, y_pred_real)
-print(f'MAE: {mae:.4f}')
+    y_pred_real = dataset.inverse_transform(y_pred_test)
+    y_test_real = dataset.inverse_transform(y_test)
 
-rmse = root_mean_squared_error(y_test_real, y_pred_real)
-print(f'RMSE: {rmse:.4f}')
+    mape = mean_absolute_percentage_error(y_test_real, y_pred_real)
+    print(f'MAPE: {mape:.4f}')
 
-model_path = 'lstm_model_weights.pth'
-torch.save(model.state_dict(),model_path)
-print(f'Model saved to {model_path}')
+    mae = mean_absolute_error(y_test_real, y_pred_real)
+    print(f'MAE: {mae:.4f}')
+
+    rmse = root_mean_squared_error(y_test_real, y_pred_real)
+    print(f'RMSE: {rmse:.4f}')
+
+    mlflow.log_metrics({"MAPE": mape,"MAE": mae,"RMSE": rmse})
+
+    model_path = 'lstm_model_weights.pth'
+    torch.save(model.state_dict(),model_path)
+    print(f'Model saved to {model_path}')
