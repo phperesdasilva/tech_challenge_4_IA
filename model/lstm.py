8699323d51e4a18a3cb4580d72e6a44b118d477e
phperesdasilva <phperesdasilva@gmail.com>
2026-05-12 22:48:16 -0300

    modulos funcionando

---
+++ model/lstm.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+
+class StockLSTM(nn.Module):
+    def __init__(self, input_size=1, hidden_size=64, num_layers=2, output_size=10, dropout=0.2):
+        super(StockLSTM, self).__init__()
+        
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        
+        self.lstm = nn.LSTM(
+            input_size, 
+            hidden_size, 
+            num_layers, 
+            batch_first=True, 
+            dropout=dropout
+        )
+
+        self.fc = nn.Linear(hidden_size, output_size)
+        
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = self.fc(out[:, -1, :])
+        return out
